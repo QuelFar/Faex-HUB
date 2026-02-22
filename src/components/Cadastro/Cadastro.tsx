9a4379bb09eec64e0b00db00fba52e67fb75e18f
QuelFar <ezefarias_2000@hotmail.com>
2026-02-22 01:56:36 -0300

    Ajustado o cadastro

--- src/components/Cadastro/Cadastro.tsx
+++ src/components/Cadastro/Cadastro.tsx
@@ -1,26 +1,16 @@
-import { useState } from "react";
 import { CadastroCard, CadastroContainer, Divider, Logo, Title } from "./Cadastro.styles";
-import Input from "./Components/Input/Input";
+import CadastroWizard from "./Components/Input/Input";
 
-export function Cadastro() {
-  const totalSteps = 2;
-  const [stepActive, setStepActive] = useState(1);
-
-  const next = () => setStepActive((s) => Math.min(s + 1, totalSteps));
-  const back = () => setStepActive((s) => Math.max(s - 1, 1));
-
+export function Cadastro() 
+{
   return (
     <CadastroContainer>
       <CadastroCard>
         <Logo src="/logo-faex-hub.png" alt="Logo FAEX Hub" />
         <Divider />
         <Title>Novo cadastro</Title>
-        <Input
-          stepActive={stepActive}
-          onNext={next}
-          onBack={back}
-          onGoToStep={setStepActive}
-        />
+
+        <CadastroWizard />
       </CadastroCard>
     </CadastroContainer>
   );
